108f290deffddf9e54e9e9ce501484e398ab9513
lafranfabian <happinesschoices123@gmail.com>
2024-10-24 09:08:20 +0700

    Add files via upload

---
+++ pertemuan5_js/embed/script.js
@@ -0,0 +1,13 @@
+document.write("Hellow World");
+window.alert("ini teks alert");//alert muncul duluan sebelum semua kerender dan muncul sekali saja
+
+
+console.info("Hello world");
+console.warn("Ini console warn");
+console.error("Ini error");
+
+const dataTable = [
+    {"id":1,"name":"andreas"},
+    {"id":2,"name":"simatupang"}
+]
+console.table(dataTable);
